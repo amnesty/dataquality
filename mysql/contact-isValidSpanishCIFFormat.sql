eea9ecee7bf858e5f4618a0cccf838cfd8e54a42
Capo <mail@carloscapote.com>
2013-09-02 11:04:15 +0200

    Add missing requirements in comment and correct indentation

--- mysql/contact-isValidSpanishCIFFormat.sql
+++ mysql/contact-isValidSpanishCIFFormat.sql
@@ -7,32 +7,35 @@ CREATE FUNCTION isValidSpanishCIFFormat( docNumber VARCHAR(15) )
     COMMENT 'Validates the format of a given string against CIF pattern'
 BEGIN
     /*
-	This function validates the format of a given string in order to
-	see if it fits with CIF format. Practically, it performs a validation
-	over a CIF, except this function doesn't check the control digit.
-	
-	This function is intended to work with CIF numbers.
+        This function validates the format of a given string in order to
+        see if it fits with CIF format. Practically, it performs a validation
+        over a CIF, except this function doesn't check the control digit.
 
-	This function is used by:
-	    - isValidSpanishDoc
-	    - isValidSpanishCIF
-	
-	This function returns:
-	    1: If specified string respects CIF format
-	    0: Otherwise
+        This function is intended to work with CIF numbers.
 
-	Usage:
-	    SELECT isValidSpanishCIFFormat( 'H24930836' )
-	Returns:
-	    1
+        This function is used by:
+            - isValidSpanishDoc
+            - isValidSpanishCIF
+
+        This function requires:
+            - respectsSpanishDocPattern
+
+        This function returns:
+            1: If specified string respects CIF format
+            0: Otherwise
+
+        Usage:
+            SELECT isValidSpanishCIFFormat( 'H24930836' )
+        Returns:
+            1
     */
 
     RETURN
-	respectsSpanishDocPattern(
-	    docNumber,
-	    '[PQSNWR][0-9][0-9][0-9][0-9][0-9][0-9][0-9][A-Z]' )
-	OR	    
-	respectsSpanishDocPattern(
-	    docNumber,
-	    '[ABCDEFGHJUV][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' );    
+        respectsSpanishDocPattern(
+            docNumber,
+            '[PQSNWR][0-9][0-9][0-9][0-9][0-9][0-9][0-9][A-Z]' )
+    OR
+        respectsSpanishDocPattern(
+            docNumber,
+            '[ABCDEFGHJUV][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' );    
 END $$
--- mysql/contact-isValidSpanishNIEFormat.sql
+++ mysql/contact-isValidSpanishNIEFormat.sql
@@ -7,27 +7,30 @@ CREATE FUNCTION isValidSpanishNIEFormat( docNumber VARCHAR(15) )
     COMMENT 'Validates the format of a given string against NIE pattern'
 BEGIN
     /*
-	This function validates the format of a given string in order to
-	see if it fits with NIE format. Practically, it performs a validation
-	over a NIE, except this function doesn't check the control digit.
-	
-	This function is intended to work with NIE numbers.
+        This function validates the format of a given string in order to
+        see if it fits with NIE format. Practically, it performs a validation
+        over a NIE, except this function doesn't check the control digit.
 
-	This function is used by:
-	    - isValidSpanishDoc
-	    - isValidSpanishNIE
+        This function is intended to work with NIE numbers.
 
-	This function returns:
-	    1: If specified string respects NIE format
-	    0: Otherwise
+        This function is used by:
+            - isValidSpanishDoc
+            - isValidSpanishNIE
 
-	Usage:
-	    SELECT isValidSpanishNIEFormat( 'X6089822C' )
-	Returns:
-	    1
+        This function requires:
+            - respectsSpanishDocPattern
+
+        This function returns:
+            1: If specified string respects NIE format
+            0: Otherwise
+
+        Usage:
+            SELECT isValidSpanishNIEFormat( 'X6089822C' )
+        Returns:
+            1
     */
 
     RETURN respectsSpanishDocPattern(
-	docNumber,
-	'[XYZT][0-9][0-9][0-9][0-9][0-9][0-9][0-9][A-Z0-9]' );
+        docNumber,
+        '[XYZT][0-9][0-9][0-9][0-9][0-9][0-9][0-9][A-Z0-9]' );
 END $$
