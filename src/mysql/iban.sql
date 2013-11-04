bb67c7b7a4f4aff05861c4cf6301b26b1cf0f832
elcapo <mail@carloscapote.com>
2013-11-04 11:59:45 +0100

    Fixes #5 in MySQL Language

--- src/mysql/iban.sql
+++ src/mysql/iban.sql
@@ -29,13 +29,41 @@ BEGIN
     DECLARE accWithoutLetters VARCHAR(64);
     DECLARE accMod97 INT;
     DECLARE digits VARCHAR(2);
-    
+
+    CREATE TEMPORARY TABLE accountLegthPerCountry (
+        countryCode VARCHAR(2),
+        accountLength INT,
+        PRIMARY KEY( countryCode )
+    );
+
+    /* Information Source: IBAN Registry about all ISO 13616-compliant
+        national IBAN formats (Release 45 – April 2013).
+        http://www.swift.com/dsp/resources/documents/IBAN_Registry.pdf */
+
+    INSERT INTO accountLegthPerCountry (countryCode, accountLength) VALUES
+        ('AL', '28'), ('AD', '24'), ('AT', '20'), ('AZ', '28'), ('BH', '22'),
+        ('BE', '16'), ('BA', '20'), ('BR', '29'), ('BG', '22'), ('CR', '21'),
+        ('HR', '21'), ('CY', '28'), ('CZ', '24'), ('DK', '18'), ('DO', '28'),
+        ('EE', '20'), ('FO', '18'), ('FI', '18'), ('FR', '27'), ('GE', '22'),
+        ('DE', '22'), ('GI', '23'), ('GR', '27'), ('GL', '18'), ('GT', '28'),
+        ('HU', '28'), ('IS', '26'), ('IE', '22'), ('IL', '23'), ('IT', '27'),
+        ('KZ', '20'), ('KW', '30'), ('LV', '21'), ('LB', '28'), ('LI', '21'),
+        ('LT', '20'), ('LU', '20'), ('MK', '19'), ('MT', '31'), ('MR', '27'),
+        ('MU', '30'), ('MC', '27'), ('MD', '24'), ('ME', '22'), ('NL', '18'),
+        ('NO', '15'), ('PK', '24'), ('PS', '29'), ('PL', '28'), ('PT', '25'),
+        ('RO', '24'), ('SM', '27'), ('SA', '24'), ('RS', '22'), ('SK', '24'),
+        ('SI', '19'), ('ES', '24'), ('SE', '24'), ('CH', '21'), ('TN', '24'),
+        ('TR', '26'), ('AE', '23'), ('GB', '22'), ('VG', '24'), ('AO', '25'),
+        ('BJ', '28'), ('BF', '27'), ('BI', '16'), ('CM', '27'), ('CV', '25'),
+        ('IR', '26'), ('CI', '28'), ('MG', '27'), ('ML', '28'), ('MZ', '25'),
+        ('SN', '28');
+
     SET countryCode = LEFT( accNumber, 2 );
 
     SET accountLength =
         ( SELECT lc.accountLength FROM accountLegthPerCountry AS lc
                 WHERE lc.countryCode = countryCode LIMIT 1);
-    
+
     IF ( LENGTH( accNumber ) = accountLength ) THEN
         /* Replace the two check digits by 00 (e.g., GB00 for the UK) and
             Move the four initial characters to the end of the string. */
@@ -60,6 +88,8 @@ BEGIN
     ELSE
         SET digits = '';
     END IF;
+
+    DROP TEMPORARY TABLE accountLegthPerCountry;
 
     RETURN digits;
 END $$
@@ -202,7 +232,7 @@ BEGIN
     DECLARE findLetter VARCHAR(1);
     DECLARE replaceWith INT;
     DECLARE i INT;
-    
+
     SET i = 0;
     SET letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     
@@ -217,32 +247,3 @@ BEGIN
     RETURN accNumber;
 END $$
 
-DROP TABLE IF EXISTS accountLegthPerCountry $$
-
-CREATE TABLE accountLegthPerCountry (
-    countryCode VARCHAR(2),
-    accountLength INT,
-    PRIMARY KEY( countryCode )
-) COMMENT 'IBAN length -in characters- per country' $$
-
-/* Information Source: IBAN Registry about all ISO 13616-compliant
-    national IBAN formats (Release 45 – April 2013).
-    http://www.swift.com/dsp/resources/documents/IBAN_Registry.pdf */
-    
-INSERT INTO accountLegthPerCountry (countryCode, accountLength) VALUES
-    ('AL', '28'), ('AD', '24'), ('AT', '20'), ('AZ', '28'), ('BH', '22'),
-    ('BE', '16'), ('BA', '20'), ('BR', '29'), ('BG', '22'), ('CR', '21'),
-    ('HR', '21'), ('CY', '28'), ('CZ', '24'), ('DK', '18'), ('DO', '28'),
-    ('EE', '20'), ('FO', '18'), ('FI', '18'), ('FR', '27'), ('GE', '22'),
-    ('DE', '22'), ('GI', '23'), ('GR', '27'), ('GL', '18'), ('GT', '28'),
-    ('HU', '28'), ('IS', '26'), ('IE', '22'), ('IL', '23'), ('IT', '27'),
-    ('KZ', '20'), ('KW', '30'), ('LV', '21'), ('LB', '28'), ('LI', '21'),
-    ('LT', '20'), ('LU', '20'), ('MK', '19'), ('MT', '31'), ('MR', '27'),
-    ('MU', '30'), ('MC', '27'), ('MD', '24'), ('ME', '22'), ('NL', '18'),
-    ('NO', '15'), ('PK', '24'), ('PS', '29'), ('PL', '28'), ('PT', '25'),
-    ('RO', '24'), ('SM', '27'), ('SA', '24'), ('RS', '22'), ('SK', '24'),
-    ('SI', '19'), ('ES', '24'), ('SE', '24'), ('CH', '21'), ('TN', '24'),
-    ('TR', '26'), ('AE', '23'), ('GB', '22'), ('VG', '24'), ('AO', '25'),
-    ('BJ', '28'), ('BF', '27'), ('BI', '16'), ('CM', '27'), ('CV', '25'),
-    ('IR', '26'), ('CI', '28'), ('MG', '27'), ('ML', '28'), ('MZ', '25'),
-    ('SN', '28') $$
